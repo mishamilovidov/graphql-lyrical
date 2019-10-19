29aadbcb5361f81b9321224448c5e6d61c7d22ff
Misha Milovidov <m.alexeyvich@gmail.com>
2019-10-19 13:09:37 -0700

    Add create song and initial form

---
+++ client/components/SongCreate.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Create a New Song</h4>
+        <form>
+          <label>Song Title:</label>
+          <input 
+            onChange={event => this.setState({ title: event.target.value })}
+            value={this.state.title}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SongCreate;
